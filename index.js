494ba911c828f17bcc7df87297edf41dd006fade
makwasi1 <39483317+makwasi1@users.noreply.github.com>
2020-04-29 11:59:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose'); //import mongoose
+
+const Book = require('./models/booksJson.js'); //has our schema
+
+// creating the connection
+mongoose.connect('mongodb://localhost/books',{userNewUrlParser:true})
+const db = mongoose.connection; //est our connection
+
+//checking the connection
+
+db.once('open',() =>{
+    console.log("connected");
+
+})
+//capturing errors
+db.on('error', () =>{
+    console.log(err);
+})
+
+//insert into our db
+const books = new Book({
+    title:"jameson",
+    author:"johson",
+    desc:"drinks"
+})
+books.save();
+
+Book.find((err,books) =>{
+    if(err){
+        console.log(err);
+    }
+console.log(books)
+})
+
